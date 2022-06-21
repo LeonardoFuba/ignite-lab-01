874c9374bb4c5860595f8939a1fac4f6e43cdc42
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2022-06-21 20:57:22 -0300

    feat: setup fonts, colors and header components

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4ljcdt56mom01yrdswgf7n9/master",
+  uri: "https://api-sa-east-1.graphcms.com/v2/cl4obkojr1qf801z24w1hfxvc/master",
   cache: new InMemoryCache(),
 });
